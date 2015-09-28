f77ca09f3747828990003c8db546a743d9c28d5e
mitchell <mitchell@tyway.solutions>
2015-09-29 01:11:51 +0200

    met edit profiles

--- Documents/Work/Tyway Solutions/API/sailsLogin/sailsLogin/api/controllers/UserController.js
+++ Documents/Work/Tyway Solutions/API/sailsLogin/sailsLogin/api/controllers/UserController.js
@@ -19,7 +19,46 @@ module.exports = {
 					}
 					return res.redirect('user/new');
 			}
-			res.json(user);
+			res.redirect('/user/show/'+user.id);
+		});
+	},
+
+	show: function(req, res, next) {
+		User.findOne(req.param('id'), function foundUser (err, user) {
+			if (err) return next(err);
+			if (!user) return next();
+			res.view({
+				user: user
+			});
+		});
+	},
+
+	index: function(req, res, next) {
+		User.find(function foundUsers(err,users){
+			if (err) return next(err);
+			res.view({
+				users: users
+			});
+		});
+	},
+
+	edit: function(req, res, next) {
+		User.findOne(req.param('id'), function foundUser (err, user) {
+			if (err) return next(err);
+			if (!user) return next();
+			res.view({
+				user: user
+			});
+		});
+	},
+
+	update: function(req, res, next) {
+		User.update(req.param('id'), req.params.all(), function userUpdated (err) {
+			if (err) {
+				return res.redirect('/user/edit/'+req.param('id'));
+			}
+			res.redirect('/user/show/'+req.param('id'));
 		});
 	}
+
 };
