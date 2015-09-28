fa31302c1a88559ee418693b6844569c29fe8a02
mitchell <mitchell@tyway.solutions>
2015-09-28 23:39:05 +0200

    lastest changes

---
+++ Documents/Work/Tyway Solutions/API/sailsLogin/sailsLogin/api/controllers/UserController.js
@@ -0,0 +1,25 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	'new' : function(req, res) {
+		res.view();
+	},
+
+	create: function (req, res, next) {
+		User.create(req.params.all(), function userCreated (err, user){
+			if (err) {
+					console.log(err);
+					req.session.flash = {
+						err: err
+					}
+					return res.redirect('user/new');
+			}
+			res.json(user);
+		});
+	}
+};
---
+++ Documents/Work/Tyway Solutions/API/sailsLogin/sailsLogin/assets/js/customValidate.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  $('.form-signin').validate({
+    rules : {
+      name: {
+        required: true
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        minlength: 6,
+        required: true
+      },
+      confirmation: {
+        minlength: 6,
+        equalTo: "#password"
+      }
+    },
+    succes: function(element) {
+      element.text('OK!').addClass(valid)
+    }
+  });
+
+});
